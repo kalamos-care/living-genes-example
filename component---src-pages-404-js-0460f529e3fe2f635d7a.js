480e3ea4bae2edbf24d376bf7b6df9040b62cd6b
Brad <bbthorson@gmail.com>
2021-01-26 15:31:04 -0500

    Updates

---
+++ component---src-pages-404-js-0460f529e3fe2f635d7a.js
@@ -0,0 +1,2 @@
+(window.__LOADABLE_LOADED_CHUNKS__=window.__LOADABLE_LOADED_CHUNKS__||[]).push([[15],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),l=n.n(a),u=n("Bl7J"),_=n("vrFN");e.default=function(){return l.a.createElement(u.a,null,l.a.createElement(_.a,{title:"404: Not found"}),l.a.createElement("h1",null,"404: Not Found"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-0460f529e3fe2f635d7a.js.map
