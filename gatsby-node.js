89b299c7c804ef99ae08f06fe8055c829deab8d8
Brad <bbthorson@gmail.com>
2021-01-26 15:04:59 -0500

    added docs folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,26 @@
  */
 
 // You can delete this file if you're not using it
+
+
+// force the build to update /docs so that the page builds on Github Pages
+const path = require("path")
+const fs = require("fs")
+
+exports.onPreInit = () => {
+    if (process.argv[2] === "build") {
+        fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+        fs.renameSync(
+            path.join(__dirname, "public"),
+            path.join(__dirname, "public_dev")
+        )
+    }
+}
+
+exports.onPostBuild = () => {
+    fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+    fs.renameSync(
+        path.join(__dirname, "public_dev"),
+        path.join(__dirname, "public")
+    )
+}
